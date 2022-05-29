0ae174989a845b1ffd95a07ee644ac01363e31f0
Mikaelisson <mikaelissongesuino@gmail.com>
2022-05-29 00:58:25 -0300

    adicionado CRUD no back-end

--- app.js
+++ app.js
@@ -21,25 +21,78 @@ db.once("error", ()=>{
 app.get('/', async (req, res)=>{
 
     try {
-        let doc = await List.find({});
-        res.send("Hello World");
+        let doc = await List.find({});        
+        console.log('=========IMPRESSÃO DO DOC=========');
+        console.log('=========Listando todas anotações=========');
+        console.log(doc);
+        console.log('==================================');
+        res.send(doc);
     } catch (error) {
         res.send(error);
     }
 });
 
-app.post('/', async (req, res)=>{
+
+app.post('/', express.urlencoded({extended: true}), async (req, res)=>{
 
     let list = new List({
-        title: "Titulo",
-        description: "Descrição"
+        title: req.body.title,
+        description: req.body.description,
     });
     try {
-        let doc = await list.save();
+        let doc = await list.save();        
+        console.log('=========IMPRESSÃO DO DOC=========');
+        console.log('=========Documento adicionado com sucesso=========');
+        console.log(doc);
+        console.log('==================================');
         res.send(doc);
     } catch (error) {
         res.send(error);
     }
+})
+app.post('/edit/:id', express.urlencoded({extended: true}), async (req, res)=>{
+    
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    let list = {};
+    list.title = req.body.title;
+    list.description = req.body.description;
+
+    try {        
+        await List.findByIdAndUpdate(id, list);
+        let doc = await List.findById(id);
+        console.log('=========IMPRESSÃO DO DOC=========');
+        console.log('=========Documento editado com sucesso=========');
+        console.log(doc);
+        console.log('==================================');
+        res.send(doc);
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+
+app.delete('/delete/:id', async (req, res)=>{
+
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    try {        
+        let doc = await List.findByIdAndRemove(id);        
+        console.log('=========IMPRESSÃO DO DOC=========');
+        console.log('=========Documento deletado com sucesso=========');
+        console.log(doc);
+        console.log('==================================');
+        res.send(doc);
+    } catch (error) {
+        res.send(error);
+    }
+
 })
 
 app.listen(PORT, ()=>{console.log("Server rodando na porta", PORT)});
