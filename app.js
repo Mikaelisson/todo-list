913d19f0c1f1744042a91577a18c53c940b9e1e4
Mikaelisson <mikaelissongesuino@gmail.com>
2022-05-30 22:53:06 -0300

    adicionado função de adicionar e editar

--- app.js
+++ app.js
@@ -25,31 +25,46 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', async (req, res)=>{
 
     try {
-        let doc = await List.find({});        
-        console.log('=========IMPRESSÃO DO DOC=========');
-        console.log('=========Listando todas anotações=========');
-        console.log(doc);
-        console.log('==================================');
-        res.render('index', {doc})
+        let doc = await List.find({});
+        doc.reverse()
+        res.render('index', {doc});
     } catch (error) {
         res.send(error);
     }
 });
+app.get('/edit/:id', async (req, res) => {
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    try {
+        let doc = await List.findById(id);
+        res.render('edit', {doc});
+    } catch (error) {
+        res.send(error);
+    }
+
+})
+app.get('/add', (req, res)=>{
+
+    try {
+        res.render('add')
+    } catch (error) {
+        res.send(error);
+    }
+})
 
 
-app.post('/', express.urlencoded({extended: true}), async (req, res)=>{
+app.post('/add', express.urlencoded({extended: true}), async (req, res)=>{
 
     let list = new List({
         title: req.body.title,
         description: req.body.description,
     });
     try {
-        let doc = await list.save();        
-        console.log('=========IMPRESSÃO DO DOC=========');
-        console.log('=========Documento adicionado com sucesso=========');
-        console.log(doc);
-        console.log('==================================');
-        res.send(doc);
+        await list.save();
+        res.redirect('/');
     } catch (error) {
         res.send(error);
     }
@@ -68,18 +83,12 @@ app.post('/edit/:id', express.urlencoded({extended: true}), async (req, res)=>{
     try {        
         await List.findByIdAndUpdate(id, list);
         let doc = await List.findById(id);
-        console.log('=========IMPRESSÃO DO DOC=========');
-        console.log('=========Documento editado com sucesso=========');
-        console.log(doc);
-        console.log('==================================');
-        res.send(doc);
+        res.redirect('/');
     } catch (error) {
         res.send(error);
     }
 })
-
-
-app.delete('/delete/:id', async (req, res)=>{
+app.post('/delete/:id', express.urlencoded({extends: true}), async (req, res)=>{
 
     let id = req.params.id;
     if(!id){
@@ -92,7 +101,7 @@ app.delete('/delete/:id', async (req, res)=>{
         console.log('=========Documento deletado com sucesso=========');
         console.log(doc);
         console.log('==================================');
-        res.send(doc);
+        res.redirect('/');
     } catch (error) {
         res.send(error);
     }
