3b73096c42cba72bda5c13db178681c25dc0aa77
Mikaelisson <mikaelissongesuino@gmail.com>
2022-06-18 23:40:22 -0300

    organizando rotas e add opção de ver mais

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 const mongoose = require("mongoose");
-const List = require('./models/List');
+const router = require('./routes/router')
 
 mongoose.connect('mongodb://localhost/list')
 
@@ -19,94 +19,11 @@ db.once("error", ()=>{
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', router)
 
 
-app.get('/', async (req, res)=>{
-
-    try {
-        let doc = await List.find({});
-        doc.reverse()
-        res.render('index', {doc});
-    } catch (error) {
-        res.send(error);
-    }
-});
-app.get('/edit/:id', async (req, res) => {
-    let id = req.params.id;
-    if(!id){
-        id = req.body.id;
-    }
-
-    try {
-        let doc = await List.findById(id);
-        res.render('edit', {doc});
-    } catch (error) {
-        res.send(error);
-    }
-
-})
-app.get('/add', (req, res)=>{
-
-    try {
-        res.render('add')
-    } catch (error) {
-        res.send(error);
-    }
-})
-
-
-app.post('/add', express.urlencoded({extended: true}), async (req, res)=>{
-
-    let list = new List({
-        title: req.body.title,
-        description: req.body.description,
-    });
-    try {
-        await list.save();
-        res.redirect('/');
-    } catch (error) {
-        res.send(error);
-    }
-})
-app.post('/edit/:id', express.urlencoded({extended: true}), async (req, res)=>{
-    
-    let id = req.params.id;
-    if(!id){
-        id = req.body.id;
-    }
-
-    let list = {};
-    list.title = req.body.title;
-    list.description = req.body.description;
-
-    try {        
-        await List.findByIdAndUpdate(id, list);
-        let doc = await List.findById(id);
-        res.redirect('/');
-    } catch (error) {
-        res.send(error);
-    }
-})
-app.post('/delete/:id', express.urlencoded({extended: true}), async (req, res)=>{
-
-    let id = req.params.id;
-    if(!id){
-        id = req.body.id;
-    }
-
-    try {        
-        let doc = await List.findByIdAndRemove(id);        
-        console.log('=========IMPRESSÃO DO DOC=========');
-        console.log('=========Documento deletado com sucesso=========');
-        console.log(doc);
-        console.log('==================================');
-        res.redirect('/');
-    } catch (error) {
-        res.send(error);
-    }
-
-})
 
  
 app.listen(PORT, ()=>{console.log("Server rodando na porta", PORT)});
---
+++ routes/router.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router();
+const List = require('../models/List');
+
+router.get('/', async (req, res)=>{
+
+    try {
+        let doc = await List.find({});
+        doc.reverse()
+        res.render('index', {doc});
+    } catch (error) {
+        res.send(error);
+    }
+});
+router.get('/edit/:id', async (req, res) => {
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    try {
+        let doc = await List.findById(id);
+        res.render('edit', {doc});
+    } catch (error) {
+        res.send(error);
+    }
+
+})
+router.get('/add', (req, res)=>{
+
+    try {
+        res.render('add')
+    } catch (error) {
+        res.send(error);
+    }
+})
+router.get('/view/:id', async (req, res)=>{
+    let id = req.params.id;
+
+    let doc = await List.findById(id)
+    res.render('view', {doc});
+})
+
+
+router.post('/add', express.urlencoded({extended: true}), async (req, res)=>{
+
+    let list = new List({
+        title: req.body.title,
+        description: req.body.description,
+    });
+    try {
+        await list.save();
+        res.redirect('/');
+    } catch (error) {
+        res.send(error);
+    }
+})
+router.post('/edit/:id', express.urlencoded({extended: true}), async (req, res)=>{
+    
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    let list = {};
+    list.title = req.body.title;
+    list.description = req.body.description;
+
+    try {        
+        await List.findByIdAndUpdate(id, list);
+        let doc = await List.findById(id);
+        res.redirect('/');
+    } catch (error) {
+        res.send(error);
+    }
+})
+router.post('/delete/:id', express.urlencoded({extended: true}), async (req, res)=>{
+
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    try {        
+        let doc = await List.findByIdAndRemove(id);        
+        console.log('=========IMPRESSÃO DO DOC=========');
+        console.log('=========Documento deletado com sucesso=========');
+        console.log(doc);
+        console.log('==================================');
+        res.redirect('/');
+    } catch (error) {
+        res.send(error);
+    }
+
+})
+
+module.exports = router
